08437ae891e243cfa1c81f7475cfc8c5f737718d
arodrigu <alejandro.rodriguez@inait.ai>
2022-03-02 16:16:19 +0100

    updating

--- tests/test_sound.py
+++ tests/test_sound.py
@@ -4,9 +4,9 @@ from torpydo.battleship import is_hit_sound
 def test_call_right_sound_fire():
     with mock.patch("torpydo.battleship.playsound") as mf:
         is_hit_sound(True)
-        mf.assert_called_once_with('sound/explosion2.mp3')
+        mf.assert_called_once_with('sound/explosion2.mp3', block=False)
 
 def test_call_right_sound_water():
     with mock.patch("torpydo.battleship.playsound") as mf:
         is_hit_sound(False)
-        mf.assert_called_once_with('sound/splash2.mp3')
+        mf.assert_called_once_with('sound/splash2.mp3', block=False)
